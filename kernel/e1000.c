20f7346de5e20bbe50f374c4e73edacfae270778
Liu Bingxu <qwe15889844242@163.com>
2026-04-29 15:15:06 +0800

    finish e1000 drive

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -19,18 +19,78 @@ static char *rx_bufs[RX_RING_SIZE];
 static volatile uint32 *regs;
 
 struct spinlock e1000_lock;
-static uint64 send_unuse = TX_RING_SIZE;
-// static uint64 send_un_cnt = 0;
-// static uint64 send_lc_cnt = 0;
-// static uint64 send_ec_cnt = 0;
-// static uint64 send_dd_cnt = 0;
-// static uint64 recv_unuse = RX_RING_SIZE;
+static uint64 send_index = 0;
+static uint64 send_unuse = (TX_RING_SIZE - 1);
+static uint64 send_un_cnt = 0;
+static uint64 send_lc_cnt = 0;
+static uint64 send_ec_cnt = 0;
+static uint64 send_dd_cnt = 0;
+static uint64 recv_index = 0;
+static uint64 recv_dd_cnt = 0;
 static struct proc *enetproc;
 
+static int enet_rx_packet(int num){
+    int process_num = 0;
+
+    while(process_num < num){
+        uint8 status = rx_ring[recv_index].status;
+        if(status & 0x1){
+            if((status & 0x2) == 0)
+                panic("enet_rx_packet : recv pactcket but is not a End Of Packet");
+            recv_dd_cnt++;
+            void *old_buf = rx_bufs[recv_index];
+            rx_bufs[recv_index] = kalloc();
+            if (!rx_bufs[recv_index])
+                panic("enet_rx_packet kalloc error");
+            rx_ring[recv_index].addr = (uint64)rx_bufs[recv_index];
+            release(&e1000_lock);
+            net_rx(old_buf, rx_ring[recv_index].length);
+            acquire(&e1000_lock);
+            rx_ring[recv_index].status = 0;
+            recv_index = ((recv_index + 1) % RX_RING_SIZE);
+            regs[E1000_RDT] = ((regs[E1000_RDT] + 1) % RX_RING_SIZE);
+            process_num++;
+        }else{
+            break;
+        }
+    };
+
+    return process_num;
+}
+
+static void enet_tx_packet(void){
+    while(1){
+        uint8 status = tx_ring[send_index].status;
+        if(status & 0x1){
+            if(status & 0x2)send_ec_cnt++;
+            if(status & 0x4)send_lc_cnt++;
+            if(status & 0x8)send_un_cnt++;
+            send_dd_cnt++;
+            tx_ring[send_index].status = 0;
+            kfree((void *)tx_ring[send_index].addr);
+            tx_ring[send_index].addr = 0;
+            send_index = ((send_index + 1) % TX_RING_SIZE);
+            send_unuse++;
+            // wakeup(&send_unuse);
+        }else{
+            break;
+        }
+    };
+}
+
+#define ENET_RX_PACKET_NUM 4
 static void enet_napi_sched(void){
     release(&myproc()->lock);
+    intr_on();
     while(1){
-        intr_on();
+        acquire(&e1000_lock);
+        int num = enet_rx_packet(ENET_RX_PACKET_NUM);
+        enet_tx_packet();
+        if(num != ENET_RX_PACKET_NUM){
+            regs[E1000_IMS] = ((1 << 7) | 1); // RXDW -- Receiver Descriptor Write Back
+            sleep(0, &e1000_lock);
+        }
+        release(&e1000_lock);
     }
 }
 
@@ -52,13 +112,9 @@ found:
     p->pid = allocpid();
     p->state = USED;
 
-    if((p->trapframe = (struct trapframe *)kalloc()) == 0){
-        panic("enet proc alloc page fail");
-    }
-
     memset(&p->context, 0, sizeof(p->context));
     p->context.ra = (uint64)enet_napi_sched;
-    p->context.sp = (uint64)p->trapframe + PGSIZE;
+    p->context.sp = (uint64)p->kstack + PGSIZE;
 
     return p;
 }
@@ -76,15 +132,15 @@ e1000_init(uint32 *xregs)
   regs = xregs;
 
   // Reset the device
-  regs[E1000_IMS] = 0; // disable interrupts
+  regs[E1000_IMC] = 0xffffffff; // redisable interrupts
   regs[E1000_CTL] |= E1000_CTL_RST;
-  regs[E1000_IMS] = 0; // redisable interrupts
+  regs[E1000_IMC] = 0xffffffff; // redisable interrupts
   __sync_synchronize();
 
   // [E1000 14.5] Transmit initialization
   memset(tx_ring, 0, sizeof(tx_ring));
   for (i = 0; i < TX_RING_SIZE; i++) {
-    tx_ring[i].status = E1000_TXD_STAT_DD;
+    tx_ring[i].status = 0;
     tx_bufs[i] = 0;
   }
   regs[E1000_TDBAL] = (uint64) tx_ring;
@@ -131,7 +187,7 @@ e1000_init(uint32 *xregs)
   // ask e1000 for receive interrupts.
   regs[E1000_RDTR] = 0; // interrupt after every received packet (no timer)
   regs[E1000_RADV] = 0; // interrupt after every packet (no timer)
-  regs[E1000_IMS] = (1 << 7); // RXDW -- Receiver Descriptor Write Back
+  regs[E1000_IMS] = ((1 << 7) | 1); // RXDW -- Receiver Descriptor Write Back
 
     struct proc *p;
     p = allocproc();
@@ -157,7 +213,9 @@ e1000_transmit(char *buf, int len)
     acquire(&e1000_lock);
     while(1){
         if(send_unuse == 0){
-            sleep(&send_unuse, &e1000_lock);
+            // sleep(&send_unuse, &e1000_lock);
+            release(&e1000_lock);
+            return -1;
         }else{
             tx_ring[regs[E1000_TDT]].addr = (uint64)buf;
             tx_ring[regs[E1000_TDT]].length = (len > 48) ? len : 48;
@@ -184,15 +242,14 @@ e1000_recv(void)
 
 }
 
-void
-e1000_intr(void)
-{
+void e1000_intr(void){
   // tell the e1000 we've seen this interrupt;
   // without this the e1000 won't raise any
   // further interrupts.
-  regs[E1000_ICR] = 0xffffffff;
-  regs[E1000_IMS] = 0; // redisable interrupts
+    regs[E1000_ICR] = 0xffffffff;
+    regs[E1000_IMC] = 0xffffffff; // redisable interrupts
 
+    acquire(&e1000_lock);
     acquire(&enetproc->lock);
     if(enetproc->state == SLEEPING) {
         enetproc->state = RUNNABLE;
@@ -200,6 +257,7 @@ e1000_intr(void)
         panic("napi proc no sleep but enet intr happen");
     }
     release(&enetproc->lock);
+    release(&e1000_lock);
 
-  e1000_recv();
+    e1000_recv();
 }
--- kernel/net.c
+++ kernel/net.c
@@ -19,12 +19,72 @@ static uint8 host_mac[ETHADDR_LEN] = { 0x52, 0x55, 0x0a, 0x00, 0x02, 0x02 };
 
 static struct spinlock netlock;
 
+#define UDP_PORT_BUF_MAX 16
+struct udp_port_info{
+    struct proc *p;
+    char *buf[UDP_PORT_BUF_MAX];
+    uint8 buf_used;
+    uint8 recv_index;
+    uint8 send_index;
+};
+
+struct udp_port_info_run {
+    struct udp_port_info_run *next;
+};
+static struct udp_port_info_run *freelist;
+
+static struct udp_port_info **udp_port_info_list[128];
+
 void
 netinit(void)
 {
   initlock(&netlock, "netlock");
+  freelist = (struct udp_port_info_run *)0;
 }
 
+static void free_udp_port_info(struct udp_port_info *info){
+    struct udp_port_info_run *r;
+
+    r = (struct udp_port_info_run*)info;
+
+    r->next = freelist;
+    freelist = r;
+}
+
+static struct udp_port_info *alloc_udp_port_info(void){
+    struct udp_port_info_run *r;
+
+    if(!freelist){
+        void *mem = kalloc();
+        if(!mem)
+            return 0;
+        memset(mem, 0, PGSIZE);
+        for(struct udp_port_info *temp = (struct udp_port_info *)mem;((uint64)temp + sizeof(struct udp_port_info)) < (uint64)mem + PGSIZE; temp++){
+            free_udp_port_info(temp);
+        }
+    }
+
+    r = freelist;
+    if(r)
+        freelist = r->next;
+
+    if(r){
+        ((struct udp_port_info *)r)->p = myproc();
+        ((struct udp_port_info *)r)->buf_used = 0;
+        ((struct udp_port_info *)r)->recv_index = 0;
+        ((struct udp_port_info *)r)->send_index = 0;
+    }
+    return (struct udp_port_info *)r;
+}
+
+static struct udp_port_info **find_udp_port_info(uint16 port){
+    struct udp_port_info **pp_udp_port_info = udp_port_info_list[port / 512];
+
+    if(!pp_udp_port_info){
+        return 0;
+    }
+    return &pp_udp_port_info[port % 512];
+}
 
 //
 // bind(int port)
@@ -38,7 +98,31 @@ sys_bind(void)
   // Your code here.
   //
 
-  return -1;
+    int port;
+
+    argint(0, &port);
+
+    acquire(&netlock);
+    struct udp_port_info **pp_udp_port_info = udp_port_info_list[port / 512];
+
+    if(!pp_udp_port_info){
+        udp_port_info_list[port / 512] = (struct udp_port_info **)kalloc();
+        if(!udp_port_info_list[port / 512])
+            panic("sys_bind udp_port_info_list don't have page");
+        pp_udp_port_info = udp_port_info_list[port / 512];
+        memset(pp_udp_port_info, 0, PGSIZE);
+    }
+    struct udp_port_info *p_udp_port_info = pp_udp_port_info[port % 512];
+    if(p_udp_port_info){
+        release(&netlock);
+        return -1;
+    }else{
+        pp_udp_port_info[port % 512] = alloc_udp_port_info();
+        if(!pp_udp_port_info[port % 512])
+            panic("sys_bind udp_port_info don't have cache");
+    }
+    release(&netlock);
+    return 0;
 }
 
 //
@@ -53,7 +137,20 @@ sys_unbind(void)
   // Optional: Your code here.
   //
 
-  return 0;
+    int port;
+
+    argint(0, &port);
+
+    acquire(&netlock);
+    struct udp_port_info **pp_udp_port_info = find_udp_port_info(port);
+    if((!pp_udp_port_info) || (!*pp_udp_port_info)){
+        release(&netlock);
+        return -1;
+    }
+    free_udp_port_info(*pp_udp_port_info);
+    *pp_udp_port_info = 0;
+    release(&netlock);
+    return 0;
 }
 
 //
@@ -77,7 +174,68 @@ sys_recv(void)
   //
   // Your code here.
   //
-  return -1;
+
+    struct proc *p = myproc();
+    int dport;
+    uint64 src;
+    uint64 sport;
+    uint64 bufaddr;
+    int maxlen;
+
+    argint(0, &dport);
+    argaddr(1, &src);
+    argaddr(2, &sport);
+    argaddr(3, &bufaddr);
+    argint(4, &maxlen);
+
+    acquire(&netlock);
+    struct udp_port_info **pp_udp_port_info = find_udp_port_info(dport);
+    if(!pp_udp_port_info){
+        release(&netlock);
+        return -1;
+    }
+    struct udp_port_info *p_udp_port_info = *pp_udp_port_info;
+    if(!p_udp_port_info){
+        release(&netlock);
+        return -1;
+    }
+    if(p_udp_port_info->p != p){
+        release(&netlock);
+        return -1;
+    }
+    if(p_udp_port_info->buf_used == 0){
+        sleep(&p_udp_port_info->buf_used, &netlock);
+    }
+    if(p_udp_port_info->buf_used == 0)
+        panic("sys_recv error wakeup");
+    uint32 recv_dst;
+    uint16 recv_dport;
+    struct eth *eth = (struct eth *) p_udp_port_info->buf[p_udp_port_info->send_index];
+    struct ip *ip = (struct ip *)(eth + 1);
+    struct udp *udp = (struct udp *)(ip + 1);
+    recv_dst   = ntohl(ip->ip_src);
+    recv_dport = ntohs(udp->sport);
+    int ret = copyout(p->pagetable, src, (char *)(&recv_dst), 4);
+    if(ret){
+        goto free_info;
+    }
+    ret = copyout(p->pagetable, sport, (char *)(&recv_dport), 2);
+    if(ret){
+        goto free_info;
+    }
+    uint64 len = (ntohs(udp->ulen) - sizeof(struct udp));
+    ret = copyout(p->pagetable, bufaddr, (char *)(udp + 1), (len > maxlen) ? maxlen : len);
+    if(!ret){
+        ret = (len > maxlen) ? maxlen : len;
+    }
+free_info:
+    p_udp_port_info->buf_used--;
+    kfree(p_udp_port_info->buf[p_udp_port_info->send_index]);
+    p_udp_port_info->buf[p_udp_port_info->send_index] = 0;
+    p_udp_port_info->send_index = ((p_udp_port_info->send_index + 1) % UDP_PORT_BUF_MAX);
+    release(&netlock);
+
+    return ret;
 }
 
 // This code is lifted from FreeBSD's ping.c, and is copyright by the Regents
@@ -181,9 +339,54 @@ sys_send(void)
     ip->ip_ttl = ttl;
     ip->ip_sum = ipsum;
 
-  e1000_transmit(buf, total);
+    int ret = e1000_transmit(buf, total);
+    if(ret){
+        kfree(buf);
+    }
 
   return 0;
+}
+
+static void udp_rx(char *buf, int len){
+    struct eth *eth = (struct eth *) buf;
+    struct ip *ip = (struct ip *)(eth + 1);
+    struct udp *udp= (struct udp *)(ip + 1);
+    acquire(&netlock);
+    struct udp_port_info **pp_udp_port_info = find_udp_port_info(ntohs(udp->dport));
+    if(!pp_udp_port_info){
+        printf("udp_rx: drop because !pp_udp_port_info, port : %d\n", ntohs(udp->dport));
+        goto error_out;
+    }
+    struct udp_port_info *p_udp_port_info = *pp_udp_port_info;
+    if(!p_udp_port_info){
+        printf("udp_rx: drop because !p_udp_port_info, port : %d\n", ntohs(udp->dport));
+        goto error_out;
+    }
+    uint16 ulen = ntohs(udp->ulen) - sizeof(struct udp);
+    if(ulen + sizeof(struct eth) + sizeof(struct ip) + sizeof(struct udp) > len){
+        printf("udp_rx: drop because ulen + sizeof(struct eth) + sizeof(struct ip) + sizeof(struct udp) > len\n");
+        goto error_out;
+    }
+    ip->ip_ttl = 0;
+    ip->ip_sum = udp->ulen;
+    unsigned short udp_csum = in_cksum((unsigned char *)&(ip->ip_ttl), 20 + ulen);
+    if(udp_csum != 0){
+        printf("udp_rx: drop because udp_csum != 0\n");
+        goto error_out;
+    }
+    if(p_udp_port_info->buf_used == UDP_PORT_BUF_MAX){
+        printf("udp_rx: drop because p_udp_port_info->buf_used == UDP_PORT_BUF_MAX\n");
+        goto error_out;
+    }
+    p_udp_port_info->buf_used++;
+    p_udp_port_info->buf[p_udp_port_info->recv_index] = buf;
+    p_udp_port_info->recv_index = ((p_udp_port_info->recv_index + 1) % UDP_PORT_BUF_MAX);
+    wakeup(&p_udp_port_info->buf_used);
+    release(&netlock);
+    return;
+error_out:
+    kfree(buf);
+    release(&netlock);
 }
 
 void
@@ -198,7 +401,25 @@ ip_rx(char *buf, int len)
   //
   // Your code here.
   //
-  
+
+    struct eth *eth = (struct eth *) buf;
+    struct ip *ip = (struct ip *)(eth + 1);
+    unsigned short ip_sum = in_cksum((unsigned char *)ip, sizeof(*ip));
+    if(ip_sum != 0){
+        kfree(buf);
+        return;
+    }
+    if(ip->ip_p == IPPROTO_ICMP){}
+    else if(ip->ip_p == IPPROTO_TCP){}
+    else if(ip->ip_p == IPPROTO_UDP){
+        if(len >= sizeof(struct eth) + sizeof(struct ip) + sizeof(struct udp))
+            udp_rx(buf, len);
+        else
+            kfree(buf);
+    }else{
+        printf("now recv a %d\n", ip->ip_p);
+        panic("ip_rx unknow IPPROTO");
+    }
 }
 
 //
@@ -244,7 +465,10 @@ arp_rx(char *inbuf)
   memmove(arp->tha, ineth->shost, ETHADDR_LEN);
   arp->tip = inarp->sip;
 
-  e1000_transmit(buf, sizeof(*eth) + sizeof(*arp));
+    int ret = e1000_transmit(buf, sizeof(*eth) + sizeof(*arp));
+    if(ret){
+        kfree(buf);
+    }
 
   kfree(inbuf);
 }
